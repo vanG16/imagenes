2174df94a4ca86ab94f545e643b0657fd508ad42
vanG16 <47192172+vanG16@users.noreply.github.com>
2021-03-11 16:31:50 -0600

    Add files via upload

---
+++ build/precache-manifest.6bdc5572209d615b0183bd1fd9bef3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8fee0a4657cbddbfc755b0e4ae0443",
+    "url": "/pract_react2/index.html"
+  },
+  {
+    "revision": "df95241ed13378b37051",
+    "url": "/pract_react2/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "20a434f3cd89dedc8886",
+    "url": "/pract_react2/static/js/2.68d01a0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pract_react2/static/js/2.68d01a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df95241ed13378b37051",
+    "url": "/pract_react2/static/js/main.e0e5fc2b.chunk.js"
+  },
+  {
+    "revision": "bfb33e356d0da183a01d",
+    "url": "/pract_react2/static/js/runtime-main.1ad2cd9c.js"
+  }
+]);
